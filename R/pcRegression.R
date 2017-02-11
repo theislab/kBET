91d54f65af56ee22164fab17380d70f67f255581
mbuttner <maren.buettner@helmholtz-muenchen.de>
2017-02-11 18:21:45 +0100

    update namespace

--- R/pcRegression.R
+++ R/pcRegression.R
@@ -1,13 +1,15 @@
-#this function does a linear model fit of principal components and a batch (categorical) variable
-# pca.data: a list as created by 'prcomp', pcRegression needs
+#' pcRegression
+#'
+#' @description pcRegression does a linear model fit of principal components and a batch (categorical) variable
+#' @param pca.data a list as created by 'prcomp', pcRegression needs
 #   * $x: the principal components (PCs, correctly: the rotated data)
 #   * $sdev: the standard deviations of the PCs
-# batch: vector with the batch covariate (for each cell)
-# tol: truncation threshold for significance level, default: 1e-16
-
+#' @param batch vector with the batch covariate (for each cell)
+#' @param tol truncation threshold for significance level, default: 1e-16
+#' @export
 pcRegression <- function(pca.data, batch, tol=1e-16){
   batch.levels <- unique(batch)
-  
+
   if(length(batch.levels)==2){
     #r2.batch.raw <- r2.batch
     correlate.fun <- function(rot.data, batch){
@@ -24,12 +26,12 @@ pcRegression <- function(pca.data, batch, tol=1e-16){
     r2.batch <- apply(pca.data$x, 2, correlate.fun, batch)
     r2.batch <- t(r2.batch)
     colnames(r2.batch) <- c('R.squared', 'p.value.lm', 'p.value.t.test')
-    
+
     r2.batch[r2.batch[,2]<tol,2] <- tol
     r2.batch[r2.batch[,3]<tol,3] <- tol
   }else{
 
- 
+
   #r2.batch.raw <- r2.batch
   correlate.fun <- function(rot.data, batch){
       a <- lm(rot.data ~ batch)
@@ -39,7 +41,7 @@ pcRegression <- function(pca.data, batch, tol=1e-16){
       return(result)
   }
   r2.batch <- apply(pca.data$x, 2, correlate.fun, batch)
-  
+
   r2.batch <- t(r2.batch)
   colnames(r2.batch) <- c('R.squared', 'p.value.lm')
   # for-loop replaced by correlate.fun and apply
@@ -48,11 +50,11 @@ pcRegression <- function(pca.data, batch, tol=1e-16){
   #    r2.batch[k,1] <- summary(a)$r.squared #coefficient of determination
   #    r2.batch[k,2] <- summary(a)$coefficients['batch',4] #p-value (significance level)
   #}
-  
+
   r2.batch[r2.batch[,2]<tol,2] <- tol
-  
+
   }
-  
+
   argmin <- which(r2.batch[,2]==min(r2.batch[,2]))
   normal <- sum(pca.data$sdev^2)
   var <- round((pca.data$sdev)^2 / normal *100,1)

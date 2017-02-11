1de7978c547bb7d71b9854b6e4e398bf34f12c05
mbuttner <maren.buettner@helmholtz-muenchen.de>
2017-02-11 16:53:35 +0100

    package not fully functional yet, working on it

---
+++ R/kBET-utils.R
@@ -0,0 +1,88 @@
+
+
+chi_batch_test <- function(knn.set, class.freq, batch, df)
+{
+  #knn.set: indices of nearest neighbours
+  #empirical frequencies in nn-environment (sample 1)
+  freq.env <- table(batch[knn.set])
+  full.classes <- rep(0, length(class.freq$class))
+  full.classes[ class.freq$class %in% names(freq.env)] <- freq.env
+  exp.freqs <- class.freq$freq*length(knn.set)
+  #compute chi-square test statistics
+  chi.sq.value <- sum((full.classes - exp.freqs)^2/exp.freqs)
+  result<- 1- pchisq(chi.sq.value, df) #p-value for the result
+  if(is.na(result)){ #I actually would like to now when 'NA' arises.
+    return(0)
+  }else{
+    return(result)
+  }
+}
+
+lrt_approximation <- function(knn.set, class.freq, batch, df)
+{
+  #knn.set: indices of nearest neighbours
+  #empirical frequencies in nn-environment (sample 1)
+  obs.env <- table(batch[knn.set]) #observed realisations of each category
+  freq.env <- obs.env/sum(obs.env) #observed 'probabilities'
+  full.classes <- rep(0, length(class.freq$class))
+  obs.classes <- class.freq$class %in% names(freq.env)
+  #for stability issues (to avoid the secret division by 0): introduce another alternative model where the observed probability
+  #is either the empirical frequency or 1/(sample size) at minimum
+  if (length(full.classes) > sum(obs.classes)){
+    dummy.count <- length(full.classes) -sum(obs.classes)
+    full.classes[obs.classes] <- obs.env/(sum(obs.env)+ dummy.count)
+    pmin <- 1/(sum(obs.env)+ dummy.count)
+    full.classes[!obs.classes] <- pmin
+  }else{
+    full.classes[ obs.classes] <- freq.env
+  }
+  exp.freqs <- class.freq$freq #expected 'probabilities'
+  #compute likelihood ratio of null and alternative hypothesis, test statistics converges to chi-square distribution
+  full.obs <- rep(0, length(class.freq$class))
+  full.obs[obs.classes] <- obs.env
+
+  lrt.value <- -2*sum(full.obs * log(exp.freqs/full.classes))
+
+  result<- 1- pchisq(lrt.value, df) #p-value for the result
+  if(is.na(result)){ #I actually would like to now when 'NA' arises.
+    return(0)
+  }else{
+    return(result)
+  }
+
+}
+#truncated normal distribution distribution function
+ptnorm <- function(x,mu,sd, a=0, b=1, alpha=0.05){
+  #this is the cumulative density of the truncated normal distribution
+  #x ~ N(mu, sd^2), but we condition on a <= x <= b
+  if(a>b){
+    warning("Lower and upper bound are interchanged.")
+    tmp <- a
+    a <- b
+    b <- tmp
+  }
+
+  if(sd<=0 | is.na(sd)) {
+    warning("Standard deviation must be positive.")
+    if (alpha<=0)
+    {
+      stop("False positive rate alpha must be positive.")
+    }
+    sd <- alpha
+  }
+  if (x<a | x>b){
+    warning("x out of bounds.")
+    cdf <- as.numeric(x>a)
+  }else{
+    alp <- pnorm((a-mu)/sd)
+    bet <- pnorm((b-mu)/sd)
+    zet <- pnorm((x-mu)/sd)
+    cdf <- (zet-alp)/(bet-alp)
+  }
+  return(cdf)
+}
+#wrapper for the multinomial exact test function
+multiNom <- function(x, y, z) {
+  z.f <- factor(z)
+  tmp <- multinomial.test(as.numeric(table(z.f[x])),y)
+  return(tmp$p.value)}
--- R/kBET.R
+++ R/kBET.R
@@ -1,16 +1,21 @@
-# This function uses a chi square test to evaluate the probability of a
-# batch effect.
+#' This function uses a chi square test to evaluate the probability of a
+#' batch effect.
 
-# df: dataset (rows: samples, columns: parameters)
-# batch: batch id for each cell
-# k0: number of nearest neighbours to test on (neighbourhood size)
-# knn: a set of nearest neighbours for each cell (optional)
-# testSize: number of data points to test, default: 10% sample size
-# heuristic: compute an optimal neighbourhood size k
-# stats: to create a statistics on batch estimates, evaluate 'stats' subsets
-# alpha: significance level
-# addTest: perform an LRT-approximation to the multinomial test AND a multinomial exact test (if appropriate)
-# plot: if stats > 10, then a boxplot of the resulting rejection rates is created
+#' df: dataset (rows: samples, columns: parameters)
+#' batch: batch id for each cell
+#' k0: number of nearest neighbours to test on (neighbourhood size)
+#' knn: a set of nearest neighbours for each cell (optional)
+#' testSize: number of data points to test, default: 10% sample size
+#' heuristic: compute an optimal neighbourhood size k
+#' stats: to create a statistics on batch estimates, evaluate 'stats' subsets
+#' alpha: significance level
+#' addTest: perform an LRT-approximation to the multinomial test AND a multinomial exact test (if appropriate)
+#' plot: if stats > 10, then a boxplot of the resulting rejection rates is created
+#' @useDynLib FNN
+#' @useDynLib ggplot2
+#' @include bisect.R
+#' @name kBET
+#' @export
 kBET <- function(df, batch, k0=NULL,knn=NULL, testSize=NULL,heuristic=FALSE, stats=100, alpha=0.05, addTest = FALSE, verbose=TRUE, plot = TRUE){
  require('ggplot2')
 
@@ -70,7 +75,7 @@ kBET <- function(df, batch, k0=NULL,knn=NULL, testSize=NULL,heuristic=FALSE, sta
   }
 
   if(heuristic==TRUE){
-    source('bisect.R')
+    #source('bisect.R')
     myfun <- function(x,df,batch, knn){
       res <- kBET(df=df, batch=batch, k0=x, knn=knn, testSize=NULL, heuristic=FALSE, stats=10, alpha=0.05, addTest = FALSE, plot=FALSE)
       result <- res$summary
@@ -341,89 +346,3 @@ kBET <- function(df, batch, k0=NULL,knn=NULL, testSize=NULL,heuristic=FALSE, sta
   return(rejection)
 }
 
-chi_batch_test <- function(knn.set, class.freq, batch, df)
-{
-  #knn.set: indices of nearest neighbours
-  #empirical frequencies in nn-environment (sample 1)
-  freq.env <- table(batch[knn.set])
-  full.classes <- rep(0, length(class.freq$class))
-  full.classes[ class.freq$class %in% names(freq.env)] <- freq.env
-  exp.freqs <- class.freq$freq*length(knn.set)
-  #compute chi-square test statistics
-  chi.sq.value <- sum((full.classes - exp.freqs)^2/exp.freqs)
-  result<- 1- pchisq(chi.sq.value, df) #p-value for the result
-  if(is.na(result)){ #I actually would like to now when 'NA' arises.
-    return(0)
-  }else{
-    return(result)
-  }
-}
-
-lrt_approximation <- function(knn.set, class.freq, batch, df)
-{
-  #knn.set: indices of nearest neighbours
-  #empirical frequencies in nn-environment (sample 1)
-  obs.env <- table(batch[knn.set]) #observed realisations of each category
-  freq.env <- obs.env/sum(obs.env) #observed 'probabilities'
-  full.classes <- rep(0, length(class.freq$class))
-  obs.classes <- class.freq$class %in% names(freq.env)
-  #for stability issues (to avoid the secret division by 0): introduce another alternative model where the observed probability
-  #is either the empirical frequency or 1/(sample size) at minimum
-  if (length(full.classes) > sum(obs.classes)){
-    dummy.count <- length(full.classes) -sum(obs.classes)
-    full.classes[obs.classes] <- obs.env/(sum(obs.env)+ dummy.count)
-    pmin <- 1/(sum(obs.env)+ dummy.count)
-    full.classes[!obs.classes] <- pmin
-  }else{
-    full.classes[ obs.classes] <- freq.env
-  }
-  exp.freqs <- class.freq$freq #expected 'probabilities'
-  #compute likelihood ratio of null and alternative hypothesis, test statistics converges to chi-square distribution
-  full.obs <- rep(0, length(class.freq$class))
-  full.obs[obs.classes] <- obs.env
-
-  lrt.value <- -2*sum(full.obs * log(exp.freqs/full.classes))
-
-  result<- 1- pchisq(lrt.value, df) #p-value for the result
-  if(is.na(result)){ #I actually would like to now when 'NA' arises.
-    return(0)
-  }else{
-    return(result)
-  }
-
-}
-#truncated normal distribution distribution function
-ptnorm <- function(x,mu,sd, a=0, b=1, alpha=0.05){
-  #this is the cumulative density of the truncated normal distribution
-  #x ~ N(mu, sd^2), but we condition on a <= x <= b
-  if(a>b){
-    warning("Lower and upper bound are interchanged.")
-    tmp <- a
-    a <- b
-    b <- tmp
-  }
-
-  if(sd<=0 | is.na(sd)) {
-    warning("Standard deviation must be positive.")
-    if (alpha<=0)
-    {
-      stop("False positive rate alpha must be positive.")
-    }
-    sd <- alpha
-  }
-  if (x<a | x>b){
-    warning("x out of bounds.")
-    cdf <- as.numeric(x>a)
-  }else{
-    alp <- pnorm((a-mu)/sd)
-    bet <- pnorm((b-mu)/sd)
-    zet <- pnorm((x-mu)/sd)
-    cdf <- (zet-alp)/(bet-alp)
-  }
-  return(cdf)
-}
-#wrapper for the multinomial exact test function
-multiNom <- function(x, y, z) {
-  z.f <- factor(z)
-  tmp <- multinomial.test(as.numeric(table(z.f[x])),y)
-  return(tmp$p.value)}

2e2b09179bba4808903d04e4425f9d5cdbeaa989
Maren Büttner <maren.buettner@helmholtz-muenchen.de>
2022-05-16 12:42:47 +0200

    Update class check for new R version

--- R/kBET.R
+++ R/kBET.R
@@ -192,7 +192,7 @@ kBET <- function(
   }
 
   #backward compatibility for knn-graph
-  if (class(knn) =='list'){
+  if(is(object, "list")){
     knn <- knn$nn.index
     if (verbose){
       cat('KNN input is a list, extracting nearest neighbour index.\n')
